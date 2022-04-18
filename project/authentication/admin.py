851ef413636cd979c0fc4c6b536418327e29876e
Fan Wang <nimadorostkar97@gmail.com>
2022-04-18 19:43:00 +0430

    done

--- project/authentication/admin.py
+++ project/authentication/admin.py
@@ -8,7 +8,7 @@ from .models import Profile
 
 #------------------------------------------------------------------------------
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user_name','phone','address')
+    list_display = ('image_tag', 'user_name','phone','address')
     search_fields = ['user_name', 'phone', 'address']
 
 admin.site.register(models.Profile, ProfileAdmin)
