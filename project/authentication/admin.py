5c36d64e349ef9a4fa409b173c665eb688199e95
Fan Wang <nimadorostkar97@gmail.com>
2022-04-18 19:37:36 +0430

    f

--- project/authentication/admin.py
+++ project/authentication/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
+from .models import Profile
 
-# Register your models here.
+
+
+
+
+#------------------------------------------------------------------------------
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user_name','phone','address')
+    search_fields = ['user_name', 'phone', 'address']
+
+admin.site.register(models.Profile, ProfileAdmin)
